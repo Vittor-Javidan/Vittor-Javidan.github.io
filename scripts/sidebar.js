8dea5af8b4140709cc8edcc11c0df50112dcb408
Vittor-Javidan <vittor.javidan.t@gmail.com>
2022-12-08 16:53:28 -0300

    Updating sidebar on main page

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,9 +2,5 @@ const sidebar = document.querySelector('.sidebar');
 const toggle = document.querySelector('.sidebar .toggle');
 
 toggle.addEventListener('click', () => {
-	
-	if (sidebar.className === 'sidebar')
-		sidebar.classList.add('open');
-	else
-		sidebar.classList.remove('open');
+	sidebar.classList.toggle('open');
 });
