b2334d4a67d5cd4e3c419281e064d3bad3ecceb4
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-02-16 21:06:13 -0300

    UPDATE: MyCetificates content added

--- components/customWindows/myCertifications.tsx
+++ components/customWindows/myCertifications.tsx
@@ -1,13 +1,120 @@
+import { certificatesData } from "@/data/certificatesData";
+import Link from "next/link";
 import Window from "../window";
 
 export default function MyCertificationsWindow(): JSX.Element {
+
+    const certificates = certificatesData.map((data, index) => (
+        <Certificate 
+            title={data.title}
+            intitution={data.intitution}
+            date={data.date}
+            certificateURL={data.certificateURL}
+            hoverMessage={data.hoverMessage}
+            key={index}
+        />
+    ))
+
     return (
         <Window
             taskbarTitle="My Certifications"
             startVisible={false}
             CSS_PositionUtilityClass="CSS_MyCertifications_Position"
         >
-            My Certifications
+		    <div
+                className={`
+                    flex flex-wrap justify-around gap-[20px]
+                `}
+            >
+                {...certificates}
+            </div>
         </Window>
+    )
+}
+
+function Certificate(props: {
+    title: string,
+    intitution: string,
+    date: string,
+    certificateURL: string,
+    hoverMessage: string
+}): JSX.Element {
+
+    let bgLink
+
+    switch (props.intitution) {
+        case "freeCodeCamp":    bgLink = "bg-[url('/freecodeCampBG.svg')]" ;break
+        case "HackerRank":      bgLink = "bg-[url('/hackerRankBG.svg')]"            ;break
+        case "UNA University":  bgLink = "bg-[url('/unaBG.svg')]"                   ;break
+        default: bgLink = ""
+    }
+
+    return (
+        <div
+            className={`
+                h-[225px] w-[375px] rounded-[10px]
+                duration-[150ms]
+
+                hover:shadow-customToggle 
+                hover:shadow-fuchsia-600
+            `}
+        >
+            <Link
+                href={props.certificateURL}
+                target={props.certificateURL !== "" ? "_blank" : ""}
+            >
+                <div
+                    className={`
+                        CSS_TextShadow
+                        h-full w-full rounded-[10px]
+                        flex flex-col justify-between
+                        border-[2px] border-solid border-gray-700
+                        ${bgLink} bg-no-repeat bg-fixed bg-opacity-10
+                        text-[2rem] text-gray-300 font-extrabold text-center
+                    `}
+                > 
+                    <div
+                        className={`
+                            h-full
+                            flex flex-col justify-around
+                        `}
+                    >
+                        <h1
+                            className={`
+                                mt-[10px]
+                                text-[3rem] 
+                            `}
+                        >{props.title}</h1>
+                        <div>
+                            <p
+                                className={`
+                                    mt-[10px]
+                                `}
+                            >
+                                {props.intitution}
+                            </p>
+                        <p
+                                className={`
+                                    mt-[10px] text-[1.6rem]
+                                `}
+                            >
+                                {props.date}
+                            </p>
+                        </div>
+                    </div>
+                    <p
+                        className={`
+                            CSS_HoverMessage
+                            mb-[10px]
+                            text-fuchsia-500
+                            opacity-0
+                            duration-[150ms]
+                        `}
+                    >
+                        {props.hoverMessage}
+                    </p>
+                </div>
+            </Link>
+        </div>
     )
 }
