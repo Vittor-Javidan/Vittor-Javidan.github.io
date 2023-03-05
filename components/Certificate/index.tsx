4832609d5da608feb2844ffe59afec5b7f5383a6
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-04 22:10:49 -0300

    REFACTOR: Certificate components
    	- Component was split into smaller components.
    	- Added a CSS file for each component inside Certificate
    	  components.

--- components/Certificate/index.tsx
+++ components/Certificate/index.tsx
@@ -1,4 +1,7 @@
-import Link from "next/link"
+import CertificateContent from "@/components/Certificate/CertificateContent";
+import CertificateInfo from "@/components/Certificate/CertificateInfo";
+import HoverMessage from "@/components/Certificate/HoverMessage";
+import Link from "next/link";
 
 export default function Certificate(props: {
     title: string
@@ -8,83 +11,25 @@ export default function Certificate(props: {
     hoverMessage: string
     islast: boolean
 }): JSX.Element {
-
-    let bgLink
-
-    switch (props.institution) {
-        case "freeCodeCamp":    bgLink = "bg-[url('/static/svg/freecodeCampBG.svg')]" ;break
-        case "HackerRank":      bgLink = "bg-[url('/static/svg/hackerRankBG.svg')]"   ;break
-        case "UNA University":  bgLink = "bg-[url('/static/svg/unaBG.svg')]"          ;break
-        default: bgLink = ""
-    }
-    
+        
     return (
-        <div
-            className={`
-                h-[225px] w-[375px] rounded-[10px]
-                duration-[150ms]
-
-                hover:shadow-customToggle 
-                hover:shadow-fuchsia-600
-            `}
+        <Link
+            className={props.islast ? "ACCESSIBILITY_lastWindowElement": ""}
+            href={props.certificateURL}
+            target={props.certificateURL !== "" ? "_blank" : ""}
         >
-            <Link
-                className={props.islast ? "ACCESSIBILITY_lastWindowElement": ""}
-                href={props.certificateURL}
-                target={props.certificateURL !== "" ? "_blank" : ""}
-            >
-                <div
-                    className={`
-                        CSS_TextShadow
-                        h-full w-full rounded-[10px]
-                        flex flex-col justify-between
-                        border-[2px] border-solid border-gray-700
-                        ${bgLink} bg-no-repeat bg-fixed bg-opacity-10
-                        text-[2rem] text-gray-300 font-extrabold text-center
-                    `}
-                > 
-                    <div
-                        className={`
-                            h-full
-                            flex flex-col justify-around
-                        `}
-                    >
-                        <h1
-                            className={`
-                                mt-[10px]
-                                text-[3rem] 
-                            `}
-                        >{props.title}</h1>
-                        <div>
-                            <p
-                                className={`
-                                    mt-[10px]
-                                `}
-                            >
-                                {props.institution}
-                            </p>
-                        <p
-                                className={`
-                                    mt-[10px] text-[1.6rem]
-                                `}
-                            >
-                                {props.date}
-                            </p>
-                        </div>
-                    </div>
-                    <p
-                        className={`
-                            CSS_HoverMessage
-                            mb-[10px]
-                            text-fuchsia-500
-                            opacity-0
-                            duration-[150ms]
-                        `}
-                    >
-                        {props.hoverMessage}
-                    </p>
-                </div>
-            </Link>
-        </div>
+            <CertificateContent
+                institution={props.institution}
+            > 
+                <CertificateInfo
+                    title={props.title}
+                    institution={props.institution}
+                    date={props.date}
+                />
+                <HoverMessage 
+                    hoverMessage={props.hoverMessage}
+                />
+            </CertificateContent>
+        </Link>
     )
 }
