7b2ecaa96513d42d4fa395027794a00ce7a42cbd
Vittor-Javidan <vittor.javidan.t@gmail.com>
2023-03-08 17:06:34 -0300

    FIX: Certificates background not displaying

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './components_Browser/**/*.{js,ts,jsx,tsx}',
+    './components_Mobile/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
